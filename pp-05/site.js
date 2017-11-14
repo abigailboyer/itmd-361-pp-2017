22a12a0a4030e779495cf4e5bab94ce8df9ecda9
abigail <abigailboyer1@gmail.com>
2017-11-13 19:57:08 -0600

    task two, changed to button and added cursor

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,4 +1,14 @@
-function ringDoorbell() {
+$(document).ready(function() {
   var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+  //turn the thing into a button ??
+  $("#doorbell").replaceWith('<button id="doorbellButton">play doorbell</button>');
+  //yes
+  $("#doorbellButton").click(function() {
+    ringDoorbell();
+  });
+  //yes ,,beautiful  Good
+
+  function ringDoorbell() {
+    doorbell.play();
+  }
+});
